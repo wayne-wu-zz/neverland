a02e7e740fe606c2610446a1efef97dd06cffe4f
Stanley Hong <hong@springeat-lm.tpcity.corp.yahoo.com>
2016-07-14 01:05:38 +0800

    borken database

--- fb_neverland/models.py
+++ fb_neverland/models.py
@@ -7,40 +7,43 @@ from django.db import models
 
 # Create your models here.
 class User( models.Model ):
-	uid = models.CharField(u'uid', max_length = 100)
-	first_name = models.CharField(u'first_name', max_length = 100)
-	last_name = models.CharField(u'last_name', max_length = 100)
-	nick_name = models.CharField(u'nick_name', max_length = 100)
-	age = models.IntegerField(u'first_name', )
-	local = models.CharField(u'first_name', max_length = 100)
-	gender = models.IntegerField(u'first_name')
-	relation_id = models.IntegerField(u'first_name')
-	preferred_age_above = models.IntegerField(u'first_name')
-	preferred_age_below = models.IntegerField(u'first_name')
-	preferred_gender = models.IntegerField(u'first_name')	
-	flag = models.BooleanField(u'flag')
-	current_relation_id = models.IntegerField
+
+	uid = models.CharField(u'uid', max_length = 100, default="")
+	first_name = models.CharField(u'first_name', max_length = 100, default="")
+	last_name = models.CharField(u'last_name', max_length = 100, default="")
+	nick_name = models.CharField(u'nick_name', max_length = 100, default="")
+	age = models.IntegerField(u'first_name', default=20)
+	location = models.CharField(u'location', max_length = 100, default="taiwan")
+	gender = models.IntegerField(u'gender', default=0)
+	relation_id = models.IntegerField(u'relation_id', default=-1)
+	preferred_age_above = models.IntegerField(u'preferred_age_above' , default = 15)
+	preferred_age_below = models.IntegerField(u'preferred_age_below' , default = 25)
+	preferred_gender = models.IntegerField(u'preferred_gender', default=0)	
+	flag = models.BooleanField(u'flag', default=False)
+	current_relation_id = models.IntegerField(u'current_relation_id',default=-1)
 
 class Profile_Pic( models.Model ):
-	id = model.IntegerField( u'id' )
-	uid = model.CharField( u'uid', max_length = 100 )
-	url = model.CharField( u'url', max_length = 100 )
+	uid = models.CharField( u'uid', max_length = 100 )
+	url = models.CharField( u'url', max_length = 100 )
 
 
 class Relation( models.Model ):
-	rid = models.AutoField(u'rid', primary_key=True)
-	uid1 = models.IntegerField(u'uid1')
-	uid2 = models.IntegerField(u'uid2')
-	msg12 = models.CharField( u'msg12', max_length = 100 )
-	msg21 = models.CharField( u'msg21', max_length = 100 )
-	img12 = model.CharField( u'img12', max_length = 200 )
-	img21 = model.CharField( u'img21', max_length = 200 )	
-	status1 = models.IntegerField( u'status1' )
-	status2 = models.IntegerField( u'status2' )
-	STATUS_NONE = 0
-	STATUS_YES  = 1
-	STATUS_NO   = 2
-	STATUS_GMM  = 3
+
+
+
+	#rid = models.AutoField(u'rid', primary_key=True)
+	uid1 = models.CharField(u'uid1', max_length=100, default = "")
+	uid2 = models.CharField(u'uid2', max_length=100, default = "")
+	msg12 = models.CharField( u'msg12', max_length = 100, default = "" )
+	msg21 = models.CharField( u'msg21', max_length = 100, default = "" )
+	img12 = models.CharField( u'img12', max_length = 200, default = "" )
+	img21 = models.CharField( u'img21', max_length = 200, default = "" )	
+	status1 = models.IntegerField( u'status1' , default = 0 )
+	status2 = models.IntegerField( u'status2' , default = 0)
+	#STATUS_NONE = 0
+	#STATUS_YES  = 1
+	#STATUS_NO   = 2
+	#STATUS_GMM  = 3
 
 
 
@@ -54,38 +57,38 @@ class Handler:
 		rid = relation['rid']
 		try:
 			result = self.relation.get( rid = rid )
-		except ObjectDoesNotExist
-			return { success: False, msg: 'rid not exists' }
+		except ObjectDoesNotExist:
+			return { 'success': False, 'msg': 'rid not exists' }
 
 		for k, v in relation.iteritems():
 			if k not in result:
-				return { success: False, msg: 'Contain invalid key ' + k }
+				return { 'success': False, 'msg': 'Contain invalid key ' + k }
 			result[k] = v
 
 		ret = result.save()
 		if ret == 1:
-			return { success: True }
+			return { 'success': True }
 		else:
-			return { success: False, msg: 'update operation failed' }   
+			return { 'success': False, 'msg': 'update operation failed' }   
 
 
 	def get_relation( self , rid ):
 		result = self.relation.get( rid = rid )
-		if !result:
-			return { success: False, msg: 'rid not exists' }
-		ret = { success: True }
+		if result is None:
+			return { 'success': False, 'msg': 'rid not exists' }
+		ret = { 'success': True }
 		ret['payload'] = result
 		return ret
 
 	def create_relation( self, uid1, uid2 ):
-		relation = Relation( uid1 = uid1, uid2 = uid2, msg12 = None 
-			, msg21 = None , status1 = None, status2 = None , img12 = None, 
-			img21 = None
-		 )
+		relation = Relation( uid1 = uid1, uid2 = uid2 )
 		relation.save()
-		return relation['rid']
+		return relation.id
 
 	def has_relation( self, uid1, uid2 ):
+		if len( self.relation.all() ) == 0:
+			return 0
+
 		tmp = self.relation.filter( uid1 = uid1 ).filter( uid2 = uid2 )
 		tmp2 = self.relation.filter( uid2 = uid1 ).filter( uid1 = uid2 )
 
@@ -101,46 +104,47 @@ class Handler:
 			return -1
 
 	def create_user( self, uid ):
-		user = User( uid = uid, first_name="Yahoo", last_name="Yahoo", age = 20 ,
-		nick_name = "Yahoo" , gender = 2 )
+		user = User( uid = uid )
 		user.save()
-		return { success: True }
+		return { 'success': True }
 
 	def check_user( self, uid ):
 		result = self.user.get( uid = uid )
-		if !result:
-			return { success: False, msg: 'User not exists' }
-		return { success: True, flag: result.flag }
+		if result is None:
+			return { 'success': False, 'msg': 'User not exists' }
+		return { 'success': True, 'flag': result.flag }
 
 	def get_user_current_rid( self, uid ):
 		result = self.user.get( uid = uid )
-		if !result:
-			return { success: False, msg: 'User not exists' }
-		return { success: True, rid: result[current_relation_id] }
+		if result is None:
+			return { 'success': False, 'msg': 'User not exists' }
+		return { 'success': True, 'rid': result[current_relation_id] }
 	
 	def update_user( self, uid , info ):
 		result = self.user.get( uid = uid )
-		if !result:
-			return { success: False, msg: 'User not exists' }
+		if result is None:
+			return { 'success': False, 'msg': 'User not exists' }
 
 		for k, v in info.iteritems():
-			if k not in result:
-				return { success: False, msg: 'Contain invalid field' + k }
-			result[k] = v
+			#if k not in result:
+			#	return { 'success': False, 'msg': 'Contain invalid field' + k }
+			setattr( result, k, v )
 
 		result.save()	
-		return { success: True }
+		return { 'success': True }
 
 	def delete_user( self, uid ):
 		result = self.user.get( uid = uid )
-		if !result:
-			return { success: False, msg: 'User not exists' }
+		if result is None:
+			return { 'success': False, 'msg': 'User not exists' }
 
 		result.delete()
-		return { success: True }
+		return { 'success': True }
 
-	def get_random_user(  ):
+	def get_random_user( self ):
 		return self.user.order_by('?').first()
+
+
 
 	def get_next_relation( self, uid ):
 		nuid = get_random_user()['uid']
@@ -150,7 +154,7 @@ class Handler:
 			round += 1
 
 		if round >= 5:
-			return { success: False, msg: 'No more match can be found' }
+			return { 'success': False, 'msg': 'No more match can be found' }
 
 		rel = has_relation( uid, nuid )
 		if rel is 0:
