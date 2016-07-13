769cea2d1306a7a8a675053ac71d61cbb0dcdf89
Wayne Wu <waynewu@yahoo-inc.com>
2016-07-13 22:42:02 +0800

    update

--- fb_neverland/views.py
+++ fb_neverland/views.py
@@ -12,13 +12,13 @@ VERIFY_TOKEN = "tinkerbell"
 FACEBOOK_GRAPH = "https://graph.facebook.com/v2.6/me"
 
 def get_started():
-    post_message_url = "%s/thread_settings?access_token=%s" %(FACEBOOK_GRAPH, PAGE_ACCESS_TOKEN)
+    post_message_url = "%s/thread_settings?access_token=%s" % (FACEBOOK_GRAPH, PAGE_ACCESS_TOKEN)
     response_msg = json.dumps({
         "setting_type":"call_to_actions",
         "thread_state":"new_thread",
         "call_to_actions":[
             {
-                "payload":"USER_DEFINED_PAYLOAD"
+                "payload":"GET_STARTED"
             }
         ]
     })
@@ -57,8 +57,7 @@ def send_image(fb_id, image_url):
     call_send_api(message_data)
 
 def send_buttons(fb_id, img):
-    post_message_url = "https://graph.facebook.com/v2.6/me/messages?access_token=%s" % (PAGE_ACCESS_TOKEN)
-    response_msg = json.dumps(
+    message_data = json.dumps(
         {"recipient": {"id": fb_id},
          "message":{ "attachment":{"type":"template","payload":{"template_type":"generic",
          "elements":[{
@@ -74,21 +73,23 @@ def send_buttons(fb_id, img):
               {
                 "type":"postback",
                 "title":"No",
-                "payload":"USER_DEFINED_PAYLOAD"
+                "payload":"USER_PRESSED_NO"
               },
               {
                 "type":"postback",
                 "title":"Give me more!",
-                "payload":"USER_DEFINED_PAYLOAD"
+                "payload":"USER_PRESSED_GIVE_ME_MORE"
               }]}]}}}})
-    status = requests.post(post_message_url, headers={"Content-Type": "application/json"}, data=response_msg)
-    pprint(status.json())
-
+    call_send_api(message_data)
 
 def handle_payload(UID, payload):
-    pprint("Handling payload")
-    if payload == "USER_PRESSED_YES":
-        send_message(UID, "You said yes!")
+    pprint("Handling payload..")
+    return {
+        "USER_PRESSED_YES" : send_message(UID, "You said yes!"),
+        "USER_PRESSED_NO" : send_message(UID, "You said no!" ),
+        "USER_PRESSED_GIVE_ME_MORE" : send_message(UID, "Give me more!"),
+        "GET_STARTED" : send_message(UID, "Welcome!")
+    }.get(payload)
 
 # Create your views here.
 class NeverlandView(generic.View):
@@ -96,6 +97,7 @@ class NeverlandView(generic.View):
     #GET request. Only called when it's hooked, by Facebook
     def get(self, request, *args, **kwargs):
         if self.request.GET['hub.verify_token'] == VERIFY_TOKEN:
+            get_started()
             return HttpResponse(self.request.GET['hub.challenge'])
         else:
             return HttpResponse('Error, invalid token')
@@ -112,29 +114,20 @@ class NeverlandView(generic.View):
         pprint(incoming_message)
 
         for entry in incoming_message['entry']:
-            pprint("Sender ID: ")
-            pprint(entry['id'])
-            if True: #user.check(UID)['success']:
-                #string RID = user.current_RID(UID)
-                for message in entry['messaging']:
-                    UID = message['sender']['id']
-                    if 'message' in message and 'text' in message['message']:
-                        msg = message['message']['text']
-                        send_message(UID, msg)
-                    if 'message' in message and 'attachments' in message['message']:
-                        pprint("Receive an image")
-                        for attachment in message['message']['attachments']:
-                            if attachment['type'] == 'image':
-                                img = attachment['payload']['url']
-                                pprint("IMAGE: %s" % img)
-                                #send_image(UID, img)
-                                send_buttons(UID, img)
-                    if 'postback' in message:
-                        handle_payload(UID, message['postback']['payload'])
-
-            else:
-                #pprint(user.check(UID)['message'])
-                send_message(message['sender']['id'], "Need Setting")
+            for message in entry['messaging']:
+                UID = message['sender']['id']
+                if 'message' in message and 'text' in message['message']:
+                    msg = message['message']['text']
+                    send_message(UID, msg)
+                if 'message' in message and 'attachments' in message['message']:
+                    pprint("Receive an image")
+                    for attachment in message['message']['attachments']:
+                        if attachment['type'] == 'image':
+                            img = attachment['payload']['url']
+                            pprint("IMAGE: %s" % img)
+                            send_buttons(UID, img)
+                if 'postback' in message:
+                    handle_payload(UID, message['postback']['payload'])
 
         return HttpResponse()
 
