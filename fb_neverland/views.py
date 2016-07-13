a54474f23a8191de57ac2c14f3256ffd91e171c0
Wayne Wu <waynewu@yahoo-inc.com>
2016-07-13 14:22:09 +0800

    post test

--- fb_neverland/views.py
+++ fb_neverland/views.py
@@ -1,12 +1,32 @@
-from django.shortcuts import render
+#from django.shortcuts import render
+
+import json, requests, random, re
+from pprint import pprint
 from django.views import generic
 from django.http.response import HttpResponse
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
 
+
+VERIFY_TOKEN = "tinkerbell"
 
 # Create your views here.
 class NeverlandView(generic.View):
     def get(self, request, *args, **kwargs):
-        if self.request.GET['hub.verify_token'] == 'tinkerbell':
+        if self.request.GET['hub.verify_token'] == VERIFY_TOKEN:
             return HttpResponse(self.request.GET['hub.challenge'])
         else:
             return HttpResponse('Error, invalid token')
+
+    @method_decorator(csrf_exempt)
+    def dispatch(self, request, *args, **kwargs):
+        return generic.View.dispatch(self, request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        incoming_message = json.loads(self.request.body.decode('utf-8'))
+        for entry in incoming_message['entry']:
+            for message in entry['entry']:
+                if 'message' in message:
+                    pprint(message)
+        return HttpResponse()
+
