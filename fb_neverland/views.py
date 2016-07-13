a877ae507b7826a0e947e4fc70b340963756c637
Janet Sung <janetsung@wildbuild-lm.tpcity.corp.yahoo.com>
2016-07-13 18:03:07 +0800

    print ID

--- fb_neverland/views.py
+++ fb_neverland/views.py
@@ -21,7 +21,7 @@ class NeverlandView(generic.View):
     @method_decorator(csrf_exempt)
     def dispatch(self, request, *args, **kwargs):
         return generic.View.dispatch(self, request, *args, **kwargs)
-
+'''
     def post(self, request, *args, **kwargs):
         pprint("Message received")
         incoming_message = json.loads(self.request.body.decode('utf-8'))
@@ -33,4 +33,25 @@ class NeverlandView(generic.View):
                     pprint("Text in Message")
                     pprint(message)
         return HttpResponse()
+'''
+
+    def post(self, request, *args, **kwargs):
+        pprint("Message received")
+        incoming_message = json.loads(self.request.body.decode('utf-8'))
+        #pprint("print incoming_message:")
+        #pprint(incoming_message)
+        for entry in incoming_message['entry']:
+            #pprint ("print entry: ")
+            #pprint (entry)
+            #pprint(entry)
+            pprint("Sender ID: ")
+            pprint(entry['id'])
+            for message in entry['messaging']:
+                #print message
+                #if 'text' in message:
+                pprint("print text message")
+                pprint(message['message']['text'])
+        return HttpResponse()
+ 
+
  
