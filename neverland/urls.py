df350ce8faf35b261d3cb9f808284b5d2fe9c169
Wayne Wu <waynewu@yahoo-inc.com>
2016-07-13 13:52:41 +0800

    udpate

--- neverland/urls.py
+++ neverland/urls.py
@@ -18,5 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^neverland/', include('fb_neverland.urls')),
+    url(r'^api/', include('fb_neverland.urls')),
 ]
