269a07823607c33db3381fb9ff7be85b61260e66
Stanley Hong <hong@springeat-lm.tpcity.corp.yahoo.com>
2016-07-14 05:50:32 +0800

    ya

--- fb_neverland/views.py
+++ fb_neverland/views.py
@@ -291,7 +291,7 @@ class NeverlandView(generic.View):
                                     img = attachment['payload']['url']
                                     pprint("IMAGE: %s" % img)
                                     item = handler.get_user(UID).temp
-                                    if item == "profile_pic"
+                                    if item == "profile_pic":  s
                                         handler.update_user(UID,{item:img,"flag":True})
                                         send_message(UID,"Done setting.")
                                     send_choice(UID, img)
