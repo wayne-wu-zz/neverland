75b9a2bcc85ada4582988bb265c2e01a0486d185
Wayne Wu <waynewu@yahoo-inc.com>
2016-07-13 18:06:38 +0800

    add post

--- fb_neverland/views.py
+++ fb_neverland/views.py
@@ -7,8 +7,15 @@ from django.http.response import HttpResponse
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
 
-
+PAGE_ACCESS_TOKEN = "EAAXffOTVZAtYBAGrTcncAZBsl96bNfOuz6h15LnHkZBnqvJmoiaha02e6mcwiIZBSFUfBpZCRm2oVZBvZCDK4onG42AFE2IarwPG9pe5uZB1chCZBFZAOqAkZBWv0kPj9vDxjmnfl4f5yW6uGRnt1wyWkJPEjIOw5eiKjH78vg2V8KtngZDZD"
 VERIFY_TOKEN = "tinkerbell"
+
+def post_facebook_message(fb_id, send_message):
+    post_message_url = "https://graph.facebook.com/v2.6/me/messages?access_token=%s", PAGE_ACCESS_TOKEN
+    response_msg = json.dumps({"recipient": {"id": fb_id}, "message": {"text": send_message}})
+    status = requests.post(post_message_url, headers={"Content-Type": "application/json"}, data=response_msg)
+    pprint(status.json())
+
 
 # Create your views here.
 class NeverlandView(generic.View):
@@ -32,5 +39,6 @@ class NeverlandView(generic.View):
                 if 'message' in message:
                     pprint("Text in Message")
                     pprint(message)
+                    post_facebook_message(message['sender']['id'], message['message']['text'])
         return HttpResponse()
  
