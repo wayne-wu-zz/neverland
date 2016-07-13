f5cee501a3afe33350687d2ec3efadc54c788336
Wayne Wu <waynewu@yahoo-inc.com>
2016-07-13 14:03:30 +0800

    update

--- fb_neverland/views.py
+++ fb_neverland/views.py
@@ -2,7 +2,11 @@ from django.shortcuts import render
 from django.views import generic
 from django.http.response import HttpResponse
 
+
 # Create your views here.
 class NeverlandView(generic.View):
     def get(self, request, *args, **kwargs):
-        return HttpResponse("Hello World!")
+        if self.request.GET['hub.verify_token'] == 'tinkerbell':
+            return HttpResponse(self.request.GET['hub.challenge'])
+        else:
+            return HttpResponse('Error, invalid token')
--- neverland/urls.py
+++ neverland/urls.py
@@ -18,5 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^api/', include('fb_neverland.urls')),
+    url(r'^webhook/', include('fb_neverland.urls')),
 ]
